38356c261d397290a9a9d33ad47d8e67ddb94ed1
malte <malte.hildebrand@gmail.com>
2019-03-29 22:20:04 +0100

    try catch for translation

--- python/main.py
+++ python/main.py
@@ -12,17 +12,23 @@ first_article.get('content')
 
 # tranlate first 500 Articles with google translate it and write it to mongo db
 
-count = 0
-for article in db.noz.find().skip(89).limit(500):
-    content = article.get('content')
-    clean_article = cleanhtml(content)
-    translation = translate(clean_article)
-    article_en = copy.deepcopy(article)
-    article_en['content_en'] = translation
-    db.noz_en.update_one(
-        article_en,
-        {'$set': {'_id': article_en['_id']}},
-        upsert=True)
-    count += 1
-    time.sleep(0.2)
-    print(count)
+repetition = 0
+article_count = 303
+while True:
+    repetition += 1
+    try:
+        for article in db.noz.find().skip(article_count).limit(2000):
+            content = article.get('content')
+            clean_article = cleanhtml(content)
+            translation = translate(clean_article)
+            article_en = copy.deepcopy(article)
+            article_en['content_en'] = translation
+            db.noz_en.update_one(
+                article_en,
+                {'$set': {'_id': article_en['_id']}},
+                upsert=True)
+            print(article_count)
+            article_count += 1
+    except Exception as e:
+        time.sleep(140)
+        print("After {} articles, {} occurred again: Starting repetition {}...".format(article_count, e, repetition))
